596c1af6990b3bd7b445e3b417765eb410b7b70e
Mishmoready <moses@missionreadyhq.com>
2024-02-24 13:45:28 +1300

    moar practice

--- javaScriptPractice/script.js
+++ javaScriptPractice/script.js
@@ -1,1 +1,27 @@
-console.log("!!My body is ready!!");
+console.log("!!My body is ready!! 🍔🍟🧀");
+
+const whoAmi = {
+  firstName: "Moses",
+  lastName: "Cook",
+  age: 36,
+  occupation: "Assistant Trainer",
+  myFavoriteFood: {
+    firstFavorite: "Korean Chicken",
+    secondFavorite: "Pizza",
+    thirdFavorite: "KFC",
+  },
+  myStatus: {
+    weight: 125,
+    height: 183,
+    armSize: 20,
+  },
+  isBusy: false,
+  toggleBusyness: function (busyStatus) {
+    this.isBusy = busyStatus;
+  },
+};
+
+console.log("This is who I am", whoAmi);
+
+var query = "isBusy";
+console.log("Do i feel like doing something:", whoAmi[query]);
