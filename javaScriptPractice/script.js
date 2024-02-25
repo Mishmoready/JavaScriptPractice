babf8276ce149048c3e94ce8878d880325b0cbd6
Mishmoready <moses@missionreadyhq.com>
2024-02-26 11:23:09 +1300

    happy days

--- javaScriptPractice/script.js
+++ javaScriptPractice/script.js
@@ -1,5 +1,6 @@
 console.log("!!My body is ready!! 🍔🍟🧀");
 
+// Object demonstration
 const whoAmi = {
   firstName: "Moses",
   lastName: "Cook",
@@ -13,15 +14,29 @@ const whoAmi = {
   myStatus: {
     weight: 125,
     height: 183,
-    armSize: 20,
+    armSize: 16,
   },
   isBusy: false,
   toggleBusyness: function (busyStatus) {
     this.isBusy = busyStatus;
   },
+
+  // Function to pass through the method
+  myGoals: function (newWeight, newHeight, newArmSize) {
+    this.myStatus.weight = newWeight;
+    this.myStatus.height = newHeight;
+    this.myStatus.armSize = newArmSize;
+  },
 };
 
 console.log("This is who I am", whoAmi);
 
-var query = "isBusy";
+let query = "isBusy";
 console.log("Do i feel like doing something:", whoAmi[query]);
+
+console.log(`my current weight is ${whoAmi.myStatus.weight}`);
+
+//Method to use from code line 24
+whoAmi.myGoals(85, 183, 20);
+
+console.log(`my new weight goal is ${whoAmi.myStatus.weight}`);
---
+++ javaScriptPractice/scriptArray.js
@@ -0,0 +1,8 @@
+let item = "fleshlight";
+
+const collection = ["Dog", 69, true, false, item];
+
+collection[2] = "OOOOOOOOOOOOOOOsh";
+collection[collection.length] = "new item me oosh";
+// collection.pop();
+console.log(collection);
